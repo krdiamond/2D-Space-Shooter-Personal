bc3e8e3523bb642a0d6d6165e19613c9bb771c53
Kristina <krdiamond@gmail.com>
2019-12-22 13:29:46 -0500

    added sprite and adjusted camera size

---
+++ Assets/KD-Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D shipRb;
+    public float moveSpeed;
+
+    private bool isMoving = false;
+    private bool isTurning = false;
+
+    private float horizontalInput;
+    public float turnSpeed;
+
+    //public ParticleSystem thrusters;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        //print(horizontalInput);
+
+        if (horizontalInput != 0)
+        {
+            isTurning = true;
+        }
+        else
+        {
+            isTurning = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isMoving == true)
+        {
+            Move();
+        }
+
+        if(isTurning == true)
+        {
+            TurnShip();
+        }
+    }
+
+    void Move()
+    {
+        //thrusters.Play();
+
+        shipRb.AddForce(transform.up * moveSpeed);
+    }
+
+    void TurnShip()
+    {
+        shipRb.AddTorque(-horizontalInput * turnSpeed);
+    }
+
+    /*void StopMoving()
+    {
+        shipRb.velocity = Vector2.zero;
+        shipRb.angularVelocity = 0.0f;
+    }
+    */
+}
